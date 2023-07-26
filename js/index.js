265891afc765fe39df3f0592b053a5247da36c42
rahil ahmadi <rahil.ahmadi2595@gmail.com>
2023-07-26 13:44:35 +0330

    for buttion got to start write a hover effect and very nice:)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const buterfly=document.querySelector('.buterfly');
 const imgaroosKhanoom=document.querySelector('.contentImg');
 const text=document.querySelector('.contentDesc');
-console.log(text);
 window.addEventListener('scroll',()=>{
     let value=window.scrollY;
     buterfly.style.right=-value*1+'px';
@@ -9,4 +8,23 @@ window.addEventListener('scroll',()=>{
     imgaroosKhanoom.style.left=-value*1.5+'px';
     imgaroosKhanoom.style.top=-value*1.5+'px';
     text.style.marginTop=-value*4.4+'px';
+})
+
+
+//button Effect
+const buttonStart=document.querySelector('.start_btn');
+buttonStart.addEventListener('mouseover',function(event){
+    console.log('hovered')
+    let x=event.clientX-event.target.offsetLeft;
+    let y=event.clientY-event.target.offsetTop;
+    const span=document.createElement('span');
+    span.classList.add('span_click_btn');
+    span.style.left=`${x}px`;
+    span.style.top=`${y}px`;
+    this.appendChild(span);
+
+    setTimeout(() => {
+        span.remove();
+    }, 1000);
+
 })
